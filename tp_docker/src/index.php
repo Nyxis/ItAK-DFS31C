4dc4fe192095fc5d6b9a03e0d23b8c8b5b360d3c
Ultraviolet33 <93035200+UltraViolet33@users.noreply.github.com>
2024-05-26 08:35:57 +0200

    add tp makefile and docker

---
+++ tp_docker/src/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php 
+
+        phpinfo();
+
+        $host = 'maria-db';
+        $db   = 'mydatabase';
+        $user = 'user';
+        $pass = 'password';
+        $port = "3306";
+        $charset = 'utf8mb4';
+
+        $options = [
+            \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
+            \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+            \PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+
+        
+        $dsn = "mysql:host=$host;dbname=$db;charset=$charset;port=$port";
+        $pdo = new \PDO($dsn, $user, $pass, $options);
+
+
+    ?>
+</body>
+</html>
